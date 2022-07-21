7685dc5957172a2fde8ff7210dabc45262379286
Mike <mikelpsv@gmail.com>
2022-07-21 14:55:32 +0300

    +create prod, mnf

--- models/product.go
+++ models/product.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"fmt"
+)
+
 // Product единица хранения
 type Product struct {
 	Id           int64        `json:"id"`
@@ -122,6 +126,74 @@ func (ps *ProductService) GetProducts() ([]Product, error) {
 	return prods, nil
 }
 
+// CreateProduct создает новый продукт
+func (ps *ProductService) CreateProduct(p *Product) (int64, error) {
+	pId := int64(0)
+	mId := int64(0)
+
+	if p.Id != 0 {
+		return 0, fmt.Errorf("possibly an error: create product with id <> 0")
+	}
+	if p.Name == "" {
+		return 0, fmt.Errorf("required field 'name' is empty")
+	}
+
+	mId = p.Manufacturer.Id
+
+	if mId == 0 && p.Manufacturer.Name != "" {
+		mnfs, err := ps.FindManufacturerByName(p.Manufacturer.Name)
+		if err != nil {
+			return 0, err
+		}
+		if len(mnfs) > 1 {
+			return 0, fmt.Errorf("it is not possible to identify the manufacturer. found %d", len(mnfs))
+		}
+		if len(mnfs) == 1 {
+			mId = mnfs[0].Id
+		}
+	}
+
+	tx, err := ps.Storage.Db.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	if mId == 0 {
+		sqlIns := "INSERT INTO manufacturers (name) VALUES ($1)"
+		err := tx.QueryRow(sqlIns, p.Manufacturer.Name).Scan(&mId)
+		if err != nil {
+			tx.Rollback()
+			return 0, err
+		}
+	}
+
+	sqlInsProd := "INSERT INTO products (name, manufacturer_id, sz_length, sz_wight, sz_height, sz_weight, sz_volume, sz_uf_volume) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING id"
+	err = tx.QueryRow(sqlInsProd, p.Name, mId, p.Size.Length, p.Size.Width, p.Size.Height, p.Size.Weight, p.Size.Volume, p.Size.UsefulVolume).Scan(&pId)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	if p.Barcodes != nil {
+		for _, bc := range p.Barcodes {
+			sqlBc := "INSERT INTO barcodes (product_id, barcode, barcode_type) " +
+				"VALUES($1, $2, $3) " +
+				"ON CONFLICT (product_id, barcode, barcode_type) DO UPDATE"
+			_, err := tx.Exec(sqlBc, pId, bc.Data, bc.Type)
+			if err != nil {
+				tx.Rollback()
+				return 0, err
+			}
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		return 0, err
+	}
+
+	return pId, nil
+}
+
 // GetManufacturers возвращает список производителей
 func (ps *ProductService) GetManufacturers() ([]Manufacturer, error) {
 	sqlMnf := "SELECT m.id, m.name FROM manufacturers m"
@@ -138,4 +210,42 @@ func (ps *ProductService) GetManufacturers() ([]Manufacturer, error) {
 		mnfs = append(mnfs, *m)
 	}
 	return mnfs, nil
+}
+
+// CreateManufacturer создает новый продукт
+func (ps *ProductService) CreateManufacturer(m *Manufacturer) (int64, error) {
+	mId := int64(0)
+	if m.Id != 0 {
+		return 0, fmt.Errorf("possibly an error: create manufacturer with id <> 0")
+	}
+	if m.Name == "" {
+		return 0, fmt.Errorf("required field 'name' is empty")
+	}
+
+	sqlInsProd := "INSERT INTO manufacturers (name) VALUES ($1) RETURNING id"
+	err := ps.Storage.Db.QueryRow(sqlInsProd, m.Name).Scan(&mId)
+	if err != nil {
+		return 0, err
+	}
+	return mId, nil
+}
+
+// FindManufacturerByName возвращает список производителей по наименованию
+func (ps *ProductService) FindManufacturerByName(mnfName string) ([]Manufacturer, error) {
+	retMnf := make([]Manufacturer, 0)
+	sql := "SELECT id, name FROM manufacturers WHERE name = $1"
+	rows, err := ps.Storage.Query(sql, mnfName)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		m := Manufacturer{}
+		err := rows.Scan(&m.Id, &m.Name)
+		if err != nil {
+			return nil, err
+		}
+		retMnf = append(retMnf, m)
+	}
+	return retMnf, nil
 }
