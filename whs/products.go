04529263bd5a675c8b83e1402820d67d745b6534
Mike <mikelpsv@gmail.com>
2023-02-20 15:37:45 +0300

    common prod create proc

--- whs/products.go
+++ whs/products.go
@@ -1,6 +1,7 @@
 package whs
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/mlplabs/microwms-core/core"
 	"strings"
@@ -8,10 +9,10 @@ import (
 
 // Product item, storage unit
 type Product struct {
-	ItemNumber   string       `json:"item_number"`
-	Barcodes     []Barcode    `json:"barcodes"`
-	Manufacturer Manufacturer `json:"manufacturer"`
-	Size         SpecificSize `json:"size"`
+	ItemNumber   string        `json:"item_number"`
+	Barcodes     []Barcode     `json:"barcodes"`
+	Manufacturer *Manufacturer `json:"manufacturer"`
+	Size         *SpecificSize `json:"size"`
 	RefItem
 }
 
@@ -163,46 +164,54 @@ func (s *Storage) GetProductsSuggestion(text string, limit int) ([]Suggestion, e
 
 // CreateProduct creates a new product
 func (s *Storage) CreateProduct(p *Product) (int64, error) {
-	pId := int64(0)
-	mId := int64(0)
-
-	if strings.TrimSpace(p.Name) == "" {
-		return 0, &core.WrapError{Err: fmt.Errorf("required field 'name' is empty"), Code: 0}
-	}
-
-	// When creating a product, we always look for a manufacturer by name, regardless of its ID
-	// because when creating, the user can choose from a list of hints and change the name
-	mId = p.Manufacturer.Id
-	mnfs, err := s.FindManufacturersByName(p.Manufacturer.Name)
-	if err != nil {
-		return 0, &core.WrapError{Err: err, Code: core.SystemError}
-	}
-	if len(mnfs) > 1 {
-		return 0, &core.WrapError{Err: fmt.Errorf("it is not possible to identify the manufacturer. found %d", len(mnfs)), Code: 0}
-	}
-	if len(mnfs) == 1 {
-		mId = mnfs[0].Id
-	}
-
-
 	tx, err := s.Db.Begin()
 	if err != nil {
 		return 0, &core.WrapError{Err: err, Code: core.SystemError}
 	}
 
+	pId, _, err := s.CreateProductInteractive(tx, p.Name, p.Manufacturer.Name, p.ItemNumber, p.Size, p.Barcodes)
+
+	err = tx.Commit()
+	if err != nil {
+		return 0, &core.WrapError{Err: err, Code: core.SystemError}
+	}
+
+	return pId, nil
+}
+
+func (s *Storage) CreateProductInteractive(tx *sql.Tx, productName, manufacturerName, itemNumber string, size *SpecificSize, barcodes []Barcode) (int64, int64, error) {
+	pId := int64(0)
+	mId := int64(0)
+
+	if strings.TrimSpace(productName) == "" {
+		return 0, 0, &core.WrapError{Err: fmt.Errorf("product name is empty"), Code: 0}
+	}
+
+	// When creating a product, we always look for a manufacturer by name, regardless of its ID
+	// because when creating, the user can choose from a list of hints and change the name
+	mnfs, err := s.FindManufacturersByName(manufacturerName)
+	if err != nil {
+		return 0, 0, &core.WrapError{Err: err, Code: core.SystemError}
+	}
+	if len(mnfs) > 1 {
+		return 0, 0, &core.WrapError{Err: fmt.Errorf("it is not possible to identify the manufacturer. found %d", len(mnfs)), Code: 0}
+	}
+	if len(mnfs) == 1 {
+		mId = mnfs[0].Id
+	}
 	// If the manufacturer is not found by name, then we create it
 	if mId == 0 {
 		sqlIns := fmt.Sprintf("INSERT INTO %s (name) VALUES ($1) RETURNING id", tableRefManufacturers)
-		err = tx.QueryRow(sqlIns, p.Manufacturer.Name).Scan(&mId)
+		err = tx.QueryRow(sqlIns, manufacturerName).Scan(&mId)
 		if err != nil {
 			tx.Rollback()
-			return 0, &core.WrapError{Err: err, Code: core.SystemError}
+			return 0, 0, &core.WrapError{Err: err, Code: core.SystemError}
 		}
 	}
-	
-	prods, err := s.FindProductsByName(p.Name)
+
+	prods, err := s.FindProductsByName(productName)
 	if err != nil {
-		return 0, &core.WrapError{Err: err, Code: core.SystemError}
+		return 0, 0, &core.WrapError{Err: err, Code: core.SystemError}
 	}
 	// можем получить несколько товаров с одинаковым имененм
 	// надо понять, имеется ли среди ниx товары с таким же производителем
@@ -216,31 +225,33 @@ func (s *Storage) CreateProduct(p *Product) (int64, error) {
 
 	// если с таким именем и производителем не нашли, то создаем новый товар
 	if pId == 0 {
+		s := SpecificSize{}
+		if size != nil {
+			s = *size
+		}
+
 		sqlInsProd := fmt.Sprintf("INSERT INTO %s (name, item_number, manufacturer_id, sz_length, sz_wight, sz_height, sz_weight, sz_volume, sz_uf_volume) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING id", tableRefProducts)
-		err = tx.QueryRow(sqlInsProd, p.Name, p.ItemNumber, mId, p.Size.Length, p.Size.Width, p.Size.Height, p.Size.Weight, p.Size.Volume, p.Size.UsefulVolume).Scan(&pId)
+		err = tx.QueryRow(sqlInsProd, productName, itemNumber, mId, s.Length, s.Width, s.Height, s.Weight, s.Volume, s.UsefulVolume).Scan(&pId)
 		if err != nil {
 			tx.Rollback()
-			return 0, &core.WrapError{Err: err, Code: core.SystemError}
+			return 0, 0, &core.WrapError{Err: err, Code: core.SystemError}
 		}
-	}
-	if p.Barcodes != nil {
-		for _, bc := range p.Barcodes {
-			sqlBc := fmt.Sprintf("INSERT INTO %s (parent_id, name, barcode_type) " +
-				"VALUES($1, $2, $3) " +
-				"ON CONFLICT (parent_id, name, barcode_type) DO UPDATE SET parent_id=$1, name=$2, barcode_type=$3", tableRefBarcodes)
-			_, err := tx.Exec(sqlBc, pId, bc.Name, bc.Type)
-			if err != nil {
-				tx.Rollback()
-				return 0, &core.WrapError{Err: err, Code: core.SystemError}
+
+		if barcodes != nil {
+			for _, bc := range barcodes {
+				sqlBc := fmt.Sprintf("INSERT INTO %s (parent_id, name, barcode_type) "+
+					"VALUES($1, $2, $3) "+
+					"ON CONFLICT (parent_id, name, barcode_type) DO UPDATE SET parent_id=$1, name=$2, barcode_type=$3", tableRefBarcodes)
+				_, err := tx.Exec(sqlBc, pId, bc.Name, bc.Type)
+				if err != nil {
+					tx.Rollback()
+					return 0, 0, &core.WrapError{Err: err, Code: core.SystemError}
+				}
 			}
 		}
-	}
-	err = tx.Commit()
-	if err != nil {
-		return 0, &core.WrapError{Err: err, Code: core.SystemError}
-	}
 
-	return pId, nil
+	}
+	return pId, mId, nil
 }
 
 // UpdateProduct updates the product
--- whs/receipt.go
+++ whs/receipt.go
@@ -1,5 +1,10 @@
 package whs
 
+import (
+	"fmt"
+	"github.com/mlplabs/microwms-core/core"
+)
+
 func (s *Storage) GetReceiptDocsItems(offset int, limit int) ([]DocItem, int, error) {
 	return s.GetDocument(docTables{
 		Headers: tableDocReceiptHeaders,
@@ -8,10 +13,98 @@ func (s *Storage) GetReceiptDocsItems(offset int, limit int) ([]DocItem, int, er
 }
 
 func (s *Storage) CreateReceiptDoc(doc *DocItem) (int64, error) {
-	return s.GetDocument(docTables{
-		Headers: tableDocReceiptHeaders,
-		Items:   tableDocReceiptItems,
-	}).createItem(doc)
+	//return s.GetDocument(docTables{
+	//	Headers: tableDocReceiptHeaders,
+	//	Items:   tableDocReceiptItems,
+	//}).createItem(doc)
+
+	tx, err := s.Db.Begin()
+	if err != nil {
+		return 0, &core.WrapError{Err: err, Code: core.SystemError}
+	}
+	sqlInsH := fmt.Sprintf("INSERT INTO %s (number, date, doc_type) VALUES($1, $2, $3) RETURNING id", tableDocReceiptItems)
+	err = tx.QueryRow(sqlInsH, doc.Number, doc.Date, DocumentTypeReceipt).Scan(&doc.Id)
+	if err != nil {
+		tx.Rollback()
+		return 0, &core.WrapError{Err: err, Code: core.SystemError}
+	}
+
+	for idx, item := range doc.Items {
+		mId := int64(0)
+		pId := int64(0)
+
+		mId = item.Product.Manufacturer.Id
+		mnfs, err := s.FindManufacturersByName(item.Product.Manufacturer.Name)
+		if err != nil {
+			return 0, &core.WrapError{Err: err, Code: core.SystemError}
+		}
+		if len(mnfs) > 1 {
+			return 0, &core.WrapError{Err: fmt.Errorf("it is not possible to identify the manufacturer. found %d", len(mnfs)), Code: 0}
+		}
+		if len(mnfs) == 1 {
+			mId = mnfs[0].Id
+		}
+
+		// If the manufacturer is not found by name, then we create it
+		if mId == 0 {
+			sqlIns := fmt.Sprintf("INSERT INTO %s (name) VALUES ($1) RETURNING id", tableRefManufacturers)
+			err = tx.QueryRow(sqlIns, item.Product.Manufacturer.Name).Scan(&mId)
+			if err != nil {
+				tx.Rollback()
+				return 0, &core.WrapError{Err: err, Code: core.SystemError}
+			}
+		}
+
+		prods, err := s.FindProductsByName(item.Product.Name)
+		if err != nil {
+			return 0, &core.WrapError{Err: err, Code: core.SystemError}
+		}
+
+		// можем получить несколько товаров с одинаковым имененм
+		// надо понять, имеется ли среди ниx товары с таким же производителем
+		for _, v := range prods {
+			if v.Manufacturer.Id == mId {
+				// нашли существующий товар
+				pId = v.Id
+				break
+			}
+		}
+
+		// если с таким именем и производителем не нашли, то создаем новый товар
+		if pId == 0 {
+			sqlInsProd := fmt.Sprintf("INSERT INTO %s (name, item_number, manufacturer_id, sz_length, sz_wight, sz_height, sz_weight, sz_volume, sz_uf_volume) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING id", tableRefProducts)
+			err = tx.QueryRow(sqlInsProd, item.Product.Name, item.Product.ItemNumber, mId, item.Product.Size.Length, item.Product.Size.Width, item.Product.Size.Height, item.Product.Size.Weight, item.Product.Size.Volume, item.Product.Size.UsefulVolume).Scan(&pId)
+			if err != nil {
+				tx.Rollback()
+				return 0, &core.WrapError{Err: err, Code: core.SystemError}
+			}
+		}
+		item.Product.Id = pId
+		item.RowId = fmt.Sprintf("%d.%d", doc.Id, idx+1)
+		sqlInsI := fmt.Sprintf("INSERT INTO %s (parent_id, row_id, product_id, quantity) VALUES($1, $2, $3, $4)", tableDocReceiptItems)
+		_, err = tx.Exec(sqlInsI, doc.Id, item.RowId, item.Product.Id, item.Quantity)
+		if err != nil {
+			tx.Rollback()
+			return 0, &core.WrapError{Err: err, Code: core.SystemError}
+		}
+
+		c := Cell{Id: 2, WhsId: 1, ZoneId: 1}
+		s := Storage{Db: s.Db}
+		item.CellDst = c
+
+		_, err = s.PutRow(doc, &item, tx)
+		if err != nil {
+			tx.Rollback()
+			return 0, &core.WrapError{Err: err, Code: core.SystemError}
+
+		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		return 0, &core.WrapError{Err: err, Code: core.SystemError}
+	}
+
+	return doc.Id, nil
 }
 
 func (s *Storage) GetReceiptDocById(id int64) (*DocItem, error) {
